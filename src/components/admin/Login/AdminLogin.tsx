cabec6439a6add226af3f9509b78bf9d9be8000e
Luca Paduano <lucapaduano09@outlook.it>
2022-08-28 10:16:34 +0200

    added login logic for admin

--- src/components/admin/Login/AdminLogin.tsx
+++ src/components/admin/Login/AdminLogin.tsx
@@ -1,17 +1,22 @@
 import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import "./AdminLogin.scss"
-
+import { useSelector, useDispatch} from "react-redux";
+import { login } from '../../../redux/adminLogged';
 
 const AdminLogin = () => {
     const [adminUser, setAdminUser] = useState("");
     const [adminPwd, setAdminPwd] = useState("");
     const history = useHistory()
+    const dispatch = useDispatch();
 
-    const checkForLogin = (name:string,pwd:string) => {
-        console.log(process.env.REACT_APP_ADMIN_USERNAME)
+    const checkForLogin = (e,name:string,pwd:string) => {
+        e.preventDefault();
         if(name === process.env.REACT_APP_ADMIN_USERNAME){
+            console.log("username valido: " + process.env.REACT_APP_ADMIN_USERNAME)
             if(pwd === process.env.REACT_APP_ADMIN_PWD){
+                console.log("password valida: " + process.env.REACT_APP_ADMIN_PWD)
+                dispatch(login());
                 history.push("/admin")
             } else {
                 window.alert("la password inserita non e' corretta")
@@ -25,7 +30,7 @@ const AdminLogin = () => {
     <div className="AdminLogin__container">
         <h1>Inserisci le tue credenziali admin</h1>
         <div className="AdminLogin__container__wrapper">
-            <form className="AdminLogin__container__wrapper__form" onSubmit={()=>checkForLogin(adminUser,adminPwd)}>
+            <form className="AdminLogin__container__wrapper__form" onSubmit={(e)=>checkForLogin(e,adminUser,adminPwd)}>
                 <input type="text" className="AdminLogin__container__wrapper__form__input"placeholder='inserisci nome admin' onChange={(e)=>setAdminUser(e.target.value)}/>
                 <input type="text" className="AdminLogin__container__wrapper__form__input"placeholder='inserisci password admin'onChange={(e)=>setAdminPwd(e.target.value)} />
                 <input type="submit" className="AdminLogin__container__wrapper__form__inputSubmit" />
