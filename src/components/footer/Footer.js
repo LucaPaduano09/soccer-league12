81cf10aa2084f4f5e7ea0826d194592e862abbeb
Luca Paduano <lucapaduano09@outlook.it>
2022-09-08 09:13:06 +0200

    si

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./footer.scss"
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="Footer__container">
@@ -8,6 +10,9 @@ const Footer = () => {
             <img src="/images/whatsapp-128.png" alt="whatsapplogo"/>
             <a href="https://wa.me/3312147601" target={"blank"}>3312147601</a>
         </div>
+        <Link to="/admin/dashboard">
+          admin
+        </Link>
     </div>
   )
 }
